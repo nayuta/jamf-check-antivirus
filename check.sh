8a41e6de716c2b87382c6fb5c58cdcd4771c360e
Nayuta Ishii <nayuta@gmail.com>
2020-04-22 18:47:05 +0900

    Fix: support multiple installation in one machine

--- check.sh
+++ check.sh
@@ -3,21 +3,26 @@
 # Add Antivirus info ("AV_NAME AV_PKG_IDENTIFIER")
 # You can check package identifier with `pkgutil --pkgs`
 AV_LIST=(
-"BitDefender com.bitdefender.antivirusformac"
+# "BitDefender com.bitdefender.antivirusformac"
 "ESET com.eset.esetEndpointAntivirus"
 "Mcafee com.mcafee.virusscan"
 "Avast com.avast.AACM"
+"CrowdStrikeFalcon com.crowdstrike.falcon.sensor"
 )
-
-RESULT="No Antivirus Detected"
 
 for _item in "${AV_LIST[@]}"
 do
   item=($_item)
 
   if [ $(pkgutil --pkgs | grep ${item[1]} | wc -l) -ge 1 ]; then
-    RESULT=${item[0]}
+    RESULT=${RESULT}${item[0]},
   fi
 done
+
+RESULT=`echo $RESULT | sed 's/\,*$//'`
+
+if [ -n $RESULT ]; then
+  RESULT='NO ANTIVIRUS DETECTED'
+fi
 
 echo "<result>$RESULT</result>"
